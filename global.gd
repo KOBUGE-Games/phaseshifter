6c79a6b43ae1e6489daa255f559da3952d137260
Johannes Danneker <philip_jay_fry@yahoo.de>
2020-12-12 21:06:15 +0100

    add color switching

---
+++ global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var full_color_palette = [Color(0,0,1),Color(1,1,0),Color(0,1,0),Color(1,0,0),Color(0,1,1),Color(1,0,1)]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- obstacle/world_creator.gd
+++ obstacle/world_creator.gd
@@ -18,6 +18,12 @@ var gap_obstacle = 0
 var probability = 0
 export var difficulty = 10
 
+# color management
+export var color_amount = 2
+var color_palette: Array
+var current_color = [Color(1,1,1),Color(1,1,1)]
+export var neutrals_color_switch = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -29,9 +35,18 @@ func _process(delta):
 
 func start_level():
 	if timer.is_stopped():
-		obstacle_switch()
+		# reset colors for every new element pair
+		current_color = [Color(1,1,1),Color(1,1,1)]
+		color_palette = global.full_color_palette.duplicate()
+		# make first tiles be flat
+		if current_horizontal < 64*10:
+			obstacle_starter()
+		else:
+			obstacle_switch()
+			
 		for x in range(2):
 			var new_obstacle = obstacle.instance()
+			new_obstacle.modulate = current_color[x]
 			new_obstacle.rotation_degrees = -x*180 # set different fade in direction for top / bottom
 			x = (x-0.5) * 2 # make value -1 and 1 to spawn top / bottom
 			new_obstacle.position = Vector2(current_horizontal,x*vertical_distance)
@@ -52,6 +67,28 @@ func obstacle_switch():
 		if probability < difficulty:
 			obstacle = obstacle1
 			previous_obstacle = 1
+			color_switch()
 		else:
 			obstacle = obstacle0
 		gap_obstacle = 1
+	if neutrals_color_switch and (obstacle == obstacle0 or obstacle == obstacle2):
+		color_switch()
+
+func obstacle_starter():
+	if gap_obstacle:
+		obstacle = obstacle2
+		gap_obstacle = 0
+	else:
+		obstacle = obstacle0
+		gap_obstacle = 1
+	if neutrals_color_switch:
+		color_switch()
+
+func color_switch():
+	var color_count = color_amount
+	for x in range(2):
+		var color_index = randi() % color_count # find a random color
+		current_color[x] = color_palette[color_index]
+		color_palette.remove(color_index) # remove color so it will not be selected twice
+		color_count -= 1
+	pass
